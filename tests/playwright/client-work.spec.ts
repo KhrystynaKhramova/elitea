39965cbdd72bd0162f4b852510d56b6fb9321d96
KhrystynaKhramova <cristinaholovchak@gmail.com>
2026-03-14 01:07:00 +0200

    Create tests/playwright/client-work.spec.ts

---
+++ tests/playwright/client-work.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM Client Work', () => {
+  test('Client Work page is visible', async ({ page }) => {
+    // TEST_BODY_PLACEHOLDER
+  });
+});
